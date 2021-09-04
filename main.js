aeced09098b0498a2b8ba17e0ec737c5a7e2b2eb
Kaylinkk <62476447+Kaylinkk@users.noreply.github.com>
2021-09-04 15:23:29 +0800

    JS:scrolling to the target

--- main.js
+++ main.js
@@ -18,3 +18,29 @@ if(window.scrollY > navbarHeight){
 }
 
 document.addEventListener("scroll", handleScroll);
+
+//handle click on nav menu
+
+const navBarMenu = document.querySelector(".navbar__menu");
+navBarMenu.addEventListener("click", (event)=>{
+    
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link === null){
+        return;
+    }
+        handleScrollToView(link);
+} );
+
+
+//handle click on "contact me"
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener("click", ()=> {
+    handleScrollToView('#contact');
+});
+
+
+function handleScrollToView(selector){
+   const scrollTo = document.querySelector(selector);
+   scrollTo.scrollIntoView({behavior:'smooth'});
+}
